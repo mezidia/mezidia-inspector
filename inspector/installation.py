6d0f268ecd36f827cd906b2ee8d4792775f9087c
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-07-08 20:36:10 +0300

    Create installation.py

---
+++ inspector/installation.py
@@ -0,0 +1,32 @@
+import gidgethub.routing
+
+from utils import get_token
+
+router = gidgethub.routing.Router()
+
+
+@router.register('installation', action='created')
+async def repo_installation_added(event, gh, *args, **kwargs):
+    """Installed bot to repository"""
+    token = await get_token(event, gh)
+    sender_name = event.data['sender']['login']
+    for repo in event.data['repositories']:
+        repo_full_name = repo['full_name']
+        if token is not None:
+            response = await gh.post(
+                f'/repos/{repo_full_name}/issues',
+                data={
+                    'title': 'Thanks for installing me!',
+                    'body': f'You are the best! @{sender_name}\n Also my creator is @mezgoodle. \
+                    There you can find my body)'
+                },
+                oauth_token=token['token'],
+            )
+            issue_url = response['url']
+            await gh.patch(
+                issue_url,
+                data={'state': 'closed'},
+                oauth_token=token['token'],
+            )
+        else:
+            await gh.post(f'/repos/{repo_full_name}/issues')
